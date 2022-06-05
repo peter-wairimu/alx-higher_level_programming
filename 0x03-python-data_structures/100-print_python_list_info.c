01fbf4483be8b44b2fec32c197dcaede2a114f9f
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-06-05 13:40:27 +0300

    initial commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * reverse - reverses the second half of the list
+ *
+ * @h_r: head of the second half
+ * Return: no return
+ */
+void print_python_list_info(PyObject *p)
+{
+    PyObject *list_len;
+    PyObject *list_sum;
+    PyObject *list_max;
+    PyObject *list_min;
+    PyObject *list_avg;
+    PyObject *list_rev;
+    PyObject *list_pal;
+    PyObject *list_rev_pal;
+    PyObject *list_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal;
+    PyObject *list_rev_pal_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal_rev_pal;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal_ rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev;
+    PyObject *list_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal_rev_pal;
