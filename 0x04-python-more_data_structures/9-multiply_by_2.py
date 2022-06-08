740c1636f517208b8a40c7714136a327ceae863e
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-06-08 20:39:35 +0300

    initial commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary is None:
-        return a_dictionary
-    if len(a_dictionary) == 0:
-        return a_dictionary
-
-    for key in a_dictionary:
-        a_dictionary[key] = a_dictionary[key] * 2
-
-    return a_dictionary
+    new_dict = a_dictionary.copy()
+    list_keys = list(new_dict.keys())
+    for key in list_keys:
+        new_dict[key] = new_dict[key] * 2
+    return new_dict
