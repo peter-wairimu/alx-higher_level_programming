bcee4bf207a63c76676d3bd853970f70136e822b
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-06-21 13:47:59 +0300

    initial commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """
-    Class Square
+    Represents a square
     """
-    def __init__(self):
-        """
-        Initialize the square
-        """
-        pass
+    pass
