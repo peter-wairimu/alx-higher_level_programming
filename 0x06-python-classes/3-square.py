53ca6bc7acd03bc5eb9a9df6f229e0b653a8ae6e
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-06-21 15:11:58 +0300

    update mandatory task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+
+class Square:
+    """Reresents a square.
+    private instance attribute: size
+    Instatiation with optional.
+    """
+
+    def __init__(self, size=0):
+        """Initializes the square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size  < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a Square class"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """Initialize the square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
