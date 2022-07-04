a1fb13c1ba507bdeac65cb00d874ad529885921e
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-07-04 14:20:07 +0300

    only sub class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-class MyList(list):
-    """ Class that inherits the attributes references of class list
-    Args:
-        list: class list
-    """
+"""Module 1-my_list.py.
+a class MyList that inherits from list.
+"""
 
+
+class MyList(list):
+    """a class MyList that inherits from list"""
 
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        """prints the list, but sorted (ascending sort)"""
+
+        newlist = self[:]
+        newlist.sort()
+        print("{}".format(newlist))
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python3
 """Module 3-is_kind_of_class.py.
-returns True if the object is exactly an instance of the specified class.
+returns True if the object is an instance of, or if the object
+false, otherwise
 """
 
 
-#!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
-    """ Function that returns True/False if obj is an instance of a_class
+    """returns True if the object is an instance of, or if the object
+    is an instance of a class that inherited from a_class
     Args:
-        obj: object
-        a_class: class type
-    Returns:
-        True if obj is an instance of a_class
-        False, otherwise
+       obj: the object to find its instance.
+       a_class: the class to look into.
+    Return: True if the obj is a_class, or if the obj is an instance
+            of a class that inherited from a_class, else False.
     """
+
     return isinstance(obj, a_class)
