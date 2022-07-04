9aca3a6ff8a1c7f323f97e517c8c372306518863
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-07-04 14:40:10 +0300

    rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,29 +4,22 @@ a class BaseGeometry with public instance methods
 """
 
 
-class BaseGeometry():
-    """a class BaseGeometry with public instance methods."""
+class BaseGeometry:
+    """ Class that defines the attributes of Geometric Shapes """
 
     def area(self):
-        """public instance that raises an Exception with the message
-        'area() is not implemented'."""
+        """ Method that defines the area of a geomtric shape """
+
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """public instance that validates value.
-        if value is not an integer:
-         raise a TypeError exception, with the message <name> must be an integer
-        if value is less or equal to 0:
-         raise a ValueError exception with the message <name> must
-         be greater than 0
-        Args:
-           name: a string.
-           value: value to validate.
+        """ Method that recieves the value property
+        Árgs:
+            name: name of the object
+            value: value of the property
         """
 
-        self.name = name
-        self.value = value
-
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        if value < 0 or value == 0:
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
