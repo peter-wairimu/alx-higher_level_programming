a5cc93d2c2cce1319cbdc79ba102b7262eedbfa2
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-07-05 18:41:51 +0300

    write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+"""
+Module that contains a function that retuns the number of lines in a file
+"""
+
+
+def write_file(filename="", text=""):
+    """ Function that writes to a file
+    Args:
+        filename: filename
+        text: text to write
+    Raises:
+        Exception: when the file can be opened
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -6,4 +6,6 @@
 | Filename | Description |
 | -------- | ----------- |
 | `0-read_file.py` | a function that reads a text file (UTF8) and prints it to stdout: |
+|`1-write_file.py`| a function that writes a string to a text file (UTF8) and returns the number of characters written: |
+
 
