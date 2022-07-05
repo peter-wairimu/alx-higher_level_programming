a1a6358f8678b724ff015dd9e561970614e0d71d
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-07-05 21:49:07 +0300

    update pycodestyle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -22,7 +22,7 @@ def pascal_triangle(n):
         triangle[i][0] = 1
         for j in range(1, i + 1):
             if j < len(triangle[i - 1]):
-                triangle[i][j] = triangle[i - 1][j - 1] +triangle[i - 1][j]
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
             else:
                 triangle[i][j] = triangle[i - 1][0]
     return triangle
