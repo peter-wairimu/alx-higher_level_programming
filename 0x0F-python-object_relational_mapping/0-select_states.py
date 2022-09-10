c0600b4c3f1614872d7ae97b7390a016cdf1e0a3
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-09-10 12:00:03 +0300

    implement pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ Lists all states from the database hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
-# grant all privileges on *.* to root@localhost identified by 'wairimu' with grant option;
+
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
