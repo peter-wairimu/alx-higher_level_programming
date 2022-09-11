7287294c0a818c068ece126224b6daa5ade726f7
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-09-11 12:43:04 +0300

    update models state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-
 """
-Contains the class definition of a State and an instance Base = declarative_base()
+Contains State class and Base, an instance of declarative_base()
 """
-
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-metadata = MetaData()
-Base = declarative_base(metadata=metadata)
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
 
 class State(Base):
     """
     Class with id and name attributes of each state
     """
-
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name = Column(String(128), nulluble = False)
+    name = Column(String(128), nullable=False)
