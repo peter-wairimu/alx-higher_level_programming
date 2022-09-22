a8dc056e000bae190bae21b9b0d4420cf299d201
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-09-22 18:40:09 +0300

    initial commit

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Pass two varibles in post method
-curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com.com&subject=I will always be here for PLD"
