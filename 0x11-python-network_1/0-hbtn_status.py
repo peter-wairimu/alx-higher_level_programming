f6513892c0c6be3e9922910e8ff1c8a39ac0ac83
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-09-23 13:35:06 +0300

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     import urllib.request
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        print("Body response:")
-        ty = response.read()
-        print("\t- type: {}".format(type(response.read())))
-        print("\t- content: {}".format(ty))
-        print("\t- utf8 content: {}".format(ty.decode('utf-8')))
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
